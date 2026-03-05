f916b903c07f0a4d8155ce09b60353212877af66
Kyorisan <contact.kyorisan@gmail.com>
2026-03-05 11:39:32 +0100

    Adding Service annotation in MedicalRecordServiceImpl

--- src/main/java/com/safetynet/alerts/service/medical/MedicalRecordServiceImpl.java
+++ src/main/java/com/safetynet/alerts/service/medical/MedicalRecordServiceImpl.java
@@ -4,10 +4,12 @@ import com.safetynet.alerts.model.MedicalRecord;
 import com.safetynet.alerts.repository.DataRepository;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MedicalRecordServiceImpl implements MedicalRecordService {
 
     private static final Logger logger = LogManager.getLogger(MedicalRecordServiceImpl.class);
