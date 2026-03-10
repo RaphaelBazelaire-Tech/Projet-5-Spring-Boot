c0c82125b648188014c01fc7a70c0451edb694ca
Kyorisan <contact.kyorisan@gmail.com>
2026-03-10 10:40:44 +0100

    Adding ChildAlertDTO

---
+++ src/main/java/com/safetynet/alerts/controller/dto/ChildAlertDTO.java
@@ -0,0 +1,24 @@
+package com.safetynet.alerts.controller.dto;
+
+import java.util.List;
+
+public class ChildAlertDTO {
+
+    private List<ChildDTO> children;
+
+    public ChildAlertDTO() {
+
+    }
+
+    public ChildAlertDTO(List<ChildDTO> children) {
+        this.children = children;
+    }
+
+    public List<ChildDTO> getChildren() {
+        return children;
+    }
+
+    public void setChildren(List<ChildDTO> children) {
+        this.children = children;
+    }
+}
