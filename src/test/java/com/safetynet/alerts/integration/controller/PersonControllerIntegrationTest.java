25670d599623be9862ec03ed67a5e3394fde9059
Kyorisan <contact.kyorisan@gmail.com>
2026-03-16 16:55:07 +0100

    Adding test methods in PersonControllerIntegrationTest

--- src/test/java/com/safetynet/alerts/integration/controller/PersonControllerIntegrationTest.java
+++ src/test/java/com/safetynet/alerts/integration/controller/PersonControllerIntegrationTest.java
@@ -1,4 +1,82 @@
 package com.safetynet.alerts.integration.controller;
 
+import com.safetynet.alerts.model.Person;
+import com.safetynet.alerts.service.person.PersonService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+import static org.mockito.Mockito.doNothing;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class PersonControllerIntegrationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private PersonService personService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void testAddPerson() throws Exception {
+
+        Person person = new Person(
+                "John",
+                "Boyd",
+                "1509 Culver St",
+                "Culver",
+                "97451",
+                "841-874-6512",
+                "john@email.com"
+        );
+
+        doNothing().when(personService).addPerson(person);
+
+        mockMvc.perform(post("/person")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(person)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testUpdatePerson() throws Exception {
+
+        Person person = new Person(
+                "John",
+                "Boyd",
+                "1509 Culver St",
+                "Culver",
+                "97451",
+                "841-874-6512",
+                "john@email.com"
+        );
+
+        doNothing().when(personService).updatePerson(person);
+
+        mockMvc.perform(put("/person")
+                .contentType("application/json")
+                .content(objectMapper.writeValueAsString(person)))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testDeletePerson() throws Exception {
+
+        doNothing().when(personService).deletePerson("John", "Boyd");
+
+        mockMvc.perform(delete("/person")
+                .param("firstName", "John")
+                .param("lastName", "Boyd"))
+                .andExpect(status().isOk());
+    }
 }
